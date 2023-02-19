d9ba0c3067c1e1ec0b04614caec0a401933d6716
Edgardo Muñoz <64887222+emm3000@users.noreply.github.com>
2023-02-19 12:15:48 -0500

    Unit Test Change

--- app/src/androidTest/java/com/emm/noteapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/emm/noteapp/ExampleInstrumentedTest.kt
@@ -16,7 +16,7 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun useAppContextx() {
+    fun useAppContextxx() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.emm.myapplication", appContext.packageName)
